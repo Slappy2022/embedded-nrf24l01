5ad6b76438ef87eca1bf4138fa9e8fe52e7f55f1
Ed <california.taliesyn@gmail.com>
2022-09-21 15:56:01 -0700

    Rename to DeviceImpl move to device.rs and refactor Nrf21l01 class to no longer template the device trait

--- src/device.rs
+++ src/device.rs
@@ -1,17 +1,142 @@
-use crate::command::Command;
-use crate::registers::{Config, Register, Status};
+use crate::command::{Command, ReadRegister, WriteRegister};
+use crate::error::Error;
+use crate::registers::{Config, Register, SetupAw, Status};
+use core::fmt::Debug;
+use embedded_hal::blocking::spi::Transfer;
+use embedded_hal::digital::v2::OutputPin;
 
-/// Trait that hides all the GPIO/SPI type parameters for use by the
+pub struct DeviceImpl<
+    Ce: OutputPin<Error = E>,
+    Csn: OutputPin<Error = E>,
+    Spi: Transfer<u8>,
+    E: Debug,
+> {
+    ce: Ce,
+    csn: Csn,
+    spi: Spi,
+    config: Config,
+}
+
+impl<
+        Ce: OutputPin<Error = E>,
+        Csn: OutputPin<Error = E>,
+        Spi: Transfer<u8, Error = SpiE>,
+        E: Debug,
+        SpiE: Debug,
+    > DeviceImpl<Ce, Csn, Spi, E>
+{
+    /// Construct a new driver instance.
+    pub fn new(mut ce: Ce, mut csn: Csn, spi: Spi) -> Result<Self, Error<SpiE>> {
+        ce.set_low().unwrap();
+        csn.set_high().unwrap();
+
+        // Reset value
+        let mut config = Config(0b0000_1000);
+        config.set_mask_rx_dr(false);
+        config.set_mask_tx_ds(false);
+        config.set_mask_max_rt(false);
+        let mut device = DeviceImpl {
+            ce,
+            csn,
+            spi,
+            config,
+        };
+
+        match device.is_connected() {
+            Err(e) => return Err(e),
+            Ok(false) => return Err(Error::NotConnected),
+            _ => {}
+        }
+        Ok(device)
+    }
+
+    /// Reads and validates content of the `SETUP_AW` register.
+    pub fn is_connected(&mut self) -> Result<bool, Error<SpiE>> {
+        let (_, setup_aw) = self.read_register::<SetupAw>()?;
+        let valid = setup_aw.aw() <= 3;
+        Ok(valid)
+    }
+}
+
+impl<
+        Ce: OutputPin<Error = E>,
+        Csn: OutputPin<Error = E>,
+        Spi: Transfer<u8, Error = SpiE>,
+        E: Debug,
+        SpiE: Debug,
+    > Device for DeviceImpl<Ce, Csn, Spi, E>
+{
+    type Error = Error<SpiE>;
+
+    fn ce_enable(&mut self) {
+        self.ce.set_high().unwrap();
+    }
+
+    fn ce_disable(&mut self) {
+        self.ce.set_low().unwrap();
+    }
+
+    fn send_command<C: Command>(
+        &mut self,
+        command: &C,
+    ) -> Result<(Status, C::Response), Self::Error> {
+        // Allocate storage
+        let mut buf_storage = [0; 33];
+        let len = command.len();
+        let buf = &mut buf_storage[0..len];
+        // Serialize the command
+        command.encode(buf);
+
+        // Spi transaction
+        self.csn.set_low().unwrap();
+        let transfer_result = self.spi.transfer(buf).map(|_| {});
+        self.csn.set_high().unwrap();
+        // Propagate Err only after csn.set_high():
+        transfer_result?;
+
+        // Parse response
+        let status = Status(buf[0]);
+        let response = C::decode_response(buf);
+
+        Ok((status, response))
+    }
+
+    fn write_register<R: Register>(&mut self, register: R) -> Result<Status, Self::Error> {
+        let (status, ()) = self.send_command(&WriteRegister::new(register))?;
+        Ok(status)
+    }
+
+    fn read_register<R: Register>(&mut self) -> Result<(Status, R), Self::Error> {
+        self.send_command(&ReadRegister::new())
+    }
+
+    fn update_config<F, R>(&mut self, f: F) -> Result<R, Self::Error>
+    where
+        F: FnOnce(&mut Config) -> R,
+    {
+        // Mutate
+        let old_config = self.config.clone();
+        let result = f(&mut self.config);
+
+        if self.config != old_config {
+            let config = self.config.clone();
+            self.write_register(config)?;
+        }
+        Ok(result)
+    }
+}
+
+/// Trait that hides all the GPIO/Spi type parameters for use by the
 /// operation modes
 pub trait Device {
-    /// Error from the SPI implementation
+    /// Error from the Spi implementation
     type Error;
 
-    /// Set CE pin high
+    /// Set Ce pin high
     fn ce_enable(&mut self);
-    /// Set CE pin low
+    /// Set Ce pin low
     fn ce_disable(&mut self);
-    /// Helper; the receiving during RX and sending during TX require `CE`
+    /// Helper; the receiving during RX and sending during TX require `Ce`
     /// to be low.
     fn with_ce_disabled<F, R>(&mut self, f: F) -> R
     where
@@ -23,8 +148,11 @@ pub trait Device {
         r
     }
 
-    /// Send a command via SPI
-    fn send_command<C: Command>(&mut self, command: &C) -> Result<(Status, C::Response), Self::Error>;
+    /// Send a command via Spi
+    fn send_command<C: Command>(
+        &mut self,
+        command: &C,
+    ) -> Result<(Status, C::Response), Self::Error>;
     /// Send `W_REGISTER` command
     fn write_register<R: Register>(&mut self, register: R) -> Result<Status, Self::Error>;
     /// Send `R_REGISTER` command
--- src/lib.rs
+++ src/lib.rs
@@ -4,28 +4,25 @@
 #[macro_use]
 extern crate bitfield;
 
-use crate::command::{FlushTx, ReadRxPayload, ReadRxPayloadWidth, WriteTxPayload};
-use crate::registers::{FifoStatus, TxAddr};
-use core::fmt;
-use core::fmt::Debug;
-use embedded_hal::blocking::spi::Transfer as SpiTransfer;
-use embedded_hal::digital::v2::OutputPin;
-
-mod config;
-pub use crate::config::{Configuration, CrcMode, DataRate};
 pub mod setup;
 
-mod registers;
-use crate::registers::{Config, Register, SetupAw, Status};
 mod command;
-use crate::command::{Command, ReadRegister, WriteRegister};
-mod payload;
-pub use crate::payload::Payload;
-mod error;
-pub use crate::error::Error;
-
+mod config;
 mod device;
-pub use crate::device::Device;
+mod error;
+mod payload;
+mod registers;
+
+pub use crate::config::{Configuration, CrcMode, DataRate};
+pub use crate::error::Error;
+pub use crate::payload::Payload;
+
+use crate::command::{FlushTx, ReadRxPayload, ReadRxPayloadWidth, WriteTxPayload};
+use crate::device::{Device, DeviceImpl};
+use crate::registers::{FifoStatus, Status};
+use core::fmt::Debug;
+use embedded_hal::blocking::spi::Transfer;
+use embedded_hal::digital::v2::OutputPin;
 
 /// Number of RX pipes with configurable addresses
 pub const PIPES_COUNT: usize = 6;
@@ -33,122 +30,6 @@ pub const PIPES_COUNT: usize = 6;
 pub const MIN_ADDR_BYTES: usize = 2;
 /// Maximum address length
 pub const MAX_ADDR_BYTES: usize = 5;
-
-/// Driver for the nRF24L01+
-///
-pub struct NRF24L01<E: Debug, CE: OutputPin<Error = E>, CSN: OutputPin<Error = E>, SPI: SpiTransfer<u8>> {
-    ce: CE,
-    csn: CSN,
-    spi: SPI,
-    config: Config,
-}
-
-impl<E: Debug, CE: OutputPin<Error = E>, CSN: OutputPin<Error = E>, SPI: SpiTransfer<u8, Error = SPIE>, SPIE: Debug> fmt::Debug
-    for NRF24L01<E, CE, CSN, SPI>
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "NRF24L01")
-    }
-}
-
-impl<E: Debug, CE: OutputPin<Error = E>, CSN: OutputPin<Error = E>, SPI: SpiTransfer<u8, Error = SPIE>, SPIE: Debug>
-    NRF24L01<E, CE, CSN, SPI>
-{
-    /// Construct a new driver instance.
-    pub fn new(mut ce: CE, mut csn: CSN, spi: SPI) -> Result<Nrf24l01<Self>, Error<SPIE>> {
-        ce.set_low().unwrap();
-        csn.set_high().unwrap();
-
-        // Reset value
-        let mut config = Config(0b0000_1000);
-        config.set_mask_rx_dr(false);
-        config.set_mask_tx_ds(false);
-        config.set_mask_max_rt(false);
-        let mut device = NRF24L01 {
-            ce,
-            csn,
-            spi,
-            config,
-        };
-
-        match device.is_connected() {
-            Err(e) => return Err(e),
-            Ok(false) => return Err(Error::NotConnected),
-            _ => {}
-        }
-        Nrf24l01::new(device)
-    }
-
-    /// Reads and validates content of the `SETUP_AW` register.
-    pub fn is_connected(&mut self) -> Result<bool, Error<SPIE>> {
-        let (_, setup_aw) = self.read_register::<SetupAw>()?;
-        let valid = setup_aw.aw() <= 3;
-        Ok(valid)
-    }
-}
-
-impl<E: Debug, CE: OutputPin<Error = E>, CSN: OutputPin<Error = E>, SPI: SpiTransfer<u8, Error = SPIE>, SPIE: Debug> Device
-    for NRF24L01<E, CE, CSN, SPI>
-{
-    type Error = Error<SPIE>;
-
-    fn ce_enable(&mut self) {
-        self.ce.set_high().unwrap();
-    }
-
-    fn ce_disable(&mut self) {
-        self.ce.set_low().unwrap();
-    }
-
-    fn send_command<C: Command>(
-        &mut self,
-        command: &C,
-    ) -> Result<(Status, C::Response), Self::Error> {
-        // Allocate storage
-        let mut buf_storage = [0; 33];
-        let len = command.len();
-        let buf = &mut buf_storage[0..len];
-        // Serialize the command
-        command.encode(buf);
-
-        // SPI transaction
-        self.csn.set_low().unwrap();
-        let transfer_result = self.spi.transfer(buf).map(|_| {});
-        self.csn.set_high().unwrap();
-        // Propagate Err only after csn.set_high():
-        transfer_result?;
-
-        // Parse response
-        let status = Status(buf[0]);
-        let response = C::decode_response(buf);
-
-        Ok((status, response))
-    }
-
-    fn write_register<R: Register>(&mut self, register: R) -> Result<Status, Self::Error> {
-        let (status, ()) = self.send_command(&WriteRegister::new(register))?;
-        Ok(status)
-    }
-
-    fn read_register<R: Register>(&mut self) -> Result<(Status, R), Self::Error> {
-        self.send_command(&ReadRegister::new())
-    }
-
-    fn update_config<F, R>(&mut self, f: F) -> Result<R, Self::Error>
-    where
-        F: FnOnce(&mut Config) -> R,
-    {
-        // Mutate
-        let old_config = self.config.clone();
-        let result = f(&mut self.config);
-
-        if self.config != old_config {
-            let config = self.config.clone();
-            self.write_register(config)?;
-        }
-        Ok(result)
-    }
-}
 
 #[derive(PartialEq)]
 enum Mode {
@@ -184,20 +65,34 @@ impl Interrupts {
     }
 }
 
-pub struct Nrf24l01<D: Device> {
+pub struct Nrf24l01<Ce, Csn, Spi, E, SpiE>
+where
+    Ce: OutputPin<Error = E>,
+    Csn: OutputPin<Error = E>,
+    Spi: Transfer<u8, Error = SpiE>,
+    E: Debug,
+    SpiE: Debug,
+{
     mode: Mode,
-    device: D,
+    device: DeviceImpl<Ce, Csn, Spi, E>,
 }
-
-impl<D: Device> Nrf24l01<D> {
-    pub fn new(mut device: D) -> Result<Self, D::Error> {
+impl<Ce, Csn, Spi, E, SpiE> Nrf24l01<Ce, Csn, Spi, E, SpiE>
+where
+    Ce: OutputPin<Error = E>,
+    Csn: OutputPin<Error = E>,
+    Spi: Transfer<u8, Error = SpiE>,
+    E: Debug,
+    SpiE: Debug,
+{
+    pub fn new(ce: Ce, csn: Csn, spi: Spi) -> Result<Self, Error<SpiE>> {
+        let mut device = DeviceImpl::new(ce, csn, spi)?;
         device.update_config(|config| config.set_pwr_up(true))?;
         Ok(Self {
             mode: Mode::Standby,
-            device
+            device,
         })
     }
-    fn clear(&mut self, interrupts: Interrupts) -> Result<(), D::Error> {
+    fn clear(&mut self, interrupts: Interrupts) -> Result<(), Error<SpiE>> {
         let mut clear = Status(0);
         clear.set_rx_dr(interrupts.rx_dr);
         clear.set_tx_ds(interrupts.tx_ds);
@@ -205,11 +100,11 @@ impl<D: Device> Nrf24l01<D> {
         self.device.write_register(clear)?;
         Ok(())
     }
-    pub fn clear_interrupts(&mut self) -> Result<(), D::Error> {
+    pub fn clear_interrupts(&mut self) -> Result<(), Error<SpiE>> {
         self.clear(Interrupts::new().set_rx_dr().set_tx_ds().set_max_rt())?;
         Ok(())
     }
-    fn rx(&mut self) -> Result<(), nb::Error<D::Error>> {
+    fn rx(&mut self) -> Result<(), nb::Error<Error<SpiE>>> {
         if self.mode == Mode::Rx {
             return Ok(());
         }
@@ -220,7 +115,7 @@ impl<D: Device> Nrf24l01<D> {
         self.mode = Mode::Rx;
         Ok(())
     }
-    fn tx(&mut self) -> Result<(), D::Error> {
+    fn tx(&mut self) -> Result<(), Error<SpiE>> {
         if self.mode == Mode::Tx {
             return Ok(());
         }
@@ -230,25 +125,25 @@ impl<D: Device> Nrf24l01<D> {
         self.mode = Mode::Tx;
         Ok(())
     }
-    pub fn send(&mut self, packet: &[u8]) -> Result<(), nb::Error<D::Error>> {
+    pub fn send(&mut self, packet: &[u8]) -> Result<(), nb::Error<Error<SpiE>>> {
         self.tx()?;
         self.device.send_command(&WriteTxPayload::new(packet))?;
         self.device.ce_enable();
         Ok(())
     }
-    fn is_tx_full(&mut self) -> Result<bool, D::Error> {
+    fn is_tx_full(&mut self) -> Result<bool, Error<SpiE>> {
         self.tx()?;
         let (_, fifo_status) = self.device.read_register::<FifoStatus>()?;
         Ok(fifo_status.tx_full())
     }
-    pub fn wait_tx_ready(&mut self) -> Result<(), nb::Error<D::Error>> {
+    pub fn wait_tx_ready(&mut self) -> Result<(), nb::Error<Error<SpiE>>> {
         self.tx()?;
         match self.is_tx_full()? {
             true => Err(nb::Error::WouldBlock),
             false => Ok(()),
         }
     }
-    pub fn wait_tx_empty(&mut self) -> Result<(), nb::Error<D::Error>> {
+    pub fn wait_tx_empty(&mut self) -> Result<(), nb::Error<Error<SpiE>>> {
         self.tx()?;
         let (status, fifo_status) = self.device.read_register::<FifoStatus>()?;
         if fifo_status.tx_empty() {
@@ -261,7 +156,7 @@ impl<D: Device> Nrf24l01<D> {
         }
         Err(nb::Error::WouldBlock)
     }
-    pub fn wait_rx_ready(&mut self) -> Result<u8, nb::Error<D::Error>> {
+    pub fn wait_rx_ready(&mut self) -> Result<u8, nb::Error<Error<SpiE>>> {
         self.rx()?;
         let (status, fifo_status) = self.device.read_register::<FifoStatus>()?;
         match fifo_status.rx_empty() {
@@ -269,7 +164,7 @@ impl<D: Device> Nrf24l01<D> {
             false => Ok(status.rx_p_no()),
         }
     }
-    pub fn read(&mut self) -> Result<Payload, nb::Error<D::Error>> {
+    pub fn read(&mut self) -> Result<Payload, nb::Error<Error<SpiE>>> {
         self.rx()?;
         let (_, payload_width) = self.device.send_command(&ReadRxPayloadWidth)?;
         let (_, payload) = self
@@ -277,14 +172,16 @@ impl<D: Device> Nrf24l01<D> {
             .send_command(&ReadRxPayload::new(payload_width as usize))?;
         Ok(payload)
     }
-    pub fn set_tx_addr(&mut self, addr: &[u8]) -> Result<(), D::Error> {
-        let register = TxAddr::new(addr);
-        self.device.write_register(register)?;
-        Ok(())
-    }
 }
-impl<D: Device> Configuration for Nrf24l01<D> {
-    type Inner = D;
+impl<Ce, Csn, Spi, E, SpiE> Configuration for Nrf24l01<Ce, Csn, Spi, E, SpiE>
+where
+    Ce: OutputPin<Error = E>,
+    Csn: OutputPin<Error = E>,
+    Spi: Transfer<u8, Error = SpiE>,
+    E: Debug,
+    SpiE: Debug,
+{
+    type Inner = DeviceImpl<Ce, Csn, Spi, E>;
     fn device(&mut self) -> &mut Self::Inner {
         &mut self.device
     }
